06409235f6f1d546de3d36f46af8974e9aaf097e
Dheeraj agrahari <93854270+agraharidheeraj@users.noreply.github.com>
2024-10-22 08:53:39 +0530

    Create meetingSlide.js

---
+++ meetingSlide.js
@@ -0,0 +1,17 @@
+const slides = [
+  {
+    image: "https://www.gstatic.com/meet/user_edu_get_a_link_light_90698cd7b4ca04d3005c962a3756c42d.svg",
+    title: "Get a link you can share",
+    description: "Click New meeting to get a link you can send to people you want to meet with",
+  },
+  {
+    image: "https://www.gstatic.com/meet/user_edu_scheduling_light_b352efa017e4f8f1ffda43e847820322.svg",
+    title: "Plan ahead",
+    description: "Click New meeting to schedule meetings in Google Calendar and send invites to participants",
+  },
+  {
+    image: "https://www.gstatic.com/meet/user_edu_safety_light_e04a2bbb449524ef7e49ea36d5f25b65.svg",
+    title: "Your meeting is safe",
+    description: "No one can join a meeting unless invited or admitted by the host",
+  },
+]
